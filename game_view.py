20f38b6df2b62f48736fcfbd0ccfc38796ab8044
ClareChannel <143736027+ClareChannel@users.noreply.github.com>
2024-09-10 19:55:31 -0500

    Add changes to game_view: inputs, class

--- game_view.py
+++ game_view.py
@@ -1,152 +1,187 @@
-## View ##
+## Battlehsip View Class ##
+#Prints boards and ships, obtains user inputs
+#takes in opponent data to be used in game
 
 ## Visual Guide ##
 # Board: 10x10 grid
-# Boat space: ^ or <
+# Boat space: ^ (V) or < (V)
 # Empty space (water): ~
 # Missed space: #
 # Boat hit: X
-# Boat sunk: *
 
-## Variable Declaration
-ship_length = [1,2,3,4,5]
-player_1_board = [["~"] * 10 for i in range(10)]
-player_2_board = [["~"] * 10 for i in range(10)]
-player_1_move_board = [["~"] * 10 for i in range(10)]
-player_2_move_board = [["~"] * 10 for i in range(10)]
-letters_to_numbers = {"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7, "I":8, "J":9}
 
-def print_grid(board):
-    #print top of board
-    print("   A B C D E F G H I J")
-    print("  +-+-+-+-+-+-+-+-+-+-+")
+class BattleshipView:
+    def __init__(self):
+        self.board_size = 10
+        self.columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+        self.rows = list(range(1, 11))
+        self.board = [['~' for _ in range(self.board_size)] for _ in range(self.board_size)]
+        self.opponent_board = [['~' for _ in range(self.board_size)] for _ in range(self.board_size)]
+        self.ship_sizes = {
+            1: 'Destroyer (1 cells)',
+            2: 'Submarine (2 cells)',
+            3: 'Cruiser (3 cells)',
+            4: 'Battleship (4 cells)',
+            5: 'Carrier (5 cells)'
+        }
 
-    #print each row of board to make grid
-    # print first 9 rows with a space before number so that it lines up with row 10 
-    row_num = 1
-    if row_num <10:
+    def print_grids(self):
+        # prints boards side by side
+        print("Your Board" + " " * 22 + "Opponent's Board")
+        print("   " + " ".join(self.columns) + " " * 9 + "   " + " ".join(self.columns))
+        for i in range(self.board_size):
+            player_row = f"{i + 1:2} " + " ".join(self.board[i])
+            opponent_row = f"{i + 1:2} " + " ".join(self.opponent_board[i])
+            print(f"{player_row}      {opponent_row}")
+
+    def convert_to_index(self, coordinate):
+        # converts coordinates into index for boards (ex. B7)
+        column = self.columns.index(coordinate[0].upper())
+        row = int(coordinate[1:]) - 1
+        return row, column
+
+    def place_ship(self, size):
+        # promts player to place ship of desired size
+        while True:
+            self.print_grids()
+            print(f"Place ship of size {size}.")
+            coordinate = input("Enter the starting coordinate (e.g., A5): ")
+            direction = input("Choose direction (H or V): ").upper()
+
+            try:
+                row, column = self.convert_to_index(coordinate)
+            except (ValueError, IndexError):
+                print("Invalid coordinate.Please try again.")
+                continue
+
+            # Place ship horizontally or vertically
+            # goes through each "box" in the given direction until length is reached
+            # checks to make sure each box is currently water (available)
+            if direction == 'H':
+                if column + size > self.board_size:
+                    print("Invalid placement. Please try again.")
+                    continue
+                if any(self.board[row][column + i] != '~' for i in range(size)):
+                    print("Invalid placement. Please try again.")
+                    continue
+                for i in range(size):
+                    self.board[row][column + i] = '<'
+            elif direction == 'V':
+                if row + size > self.board_size:
+                    print("Invalid placement. Please try again.")
+                    continue
+                if any(self.board[row + i][column] != '~' for i in range(size)):
+                    print("Invalid placement. Please try again.")
+                    continue
+                for i in range(size):
+                    self.board[row + i][column] = '^'
+            else:
+                print("Invalid direction. Please try again.")
+                continue
+            break
+
+    def num_of_ships(self):
+        # asks user how many ships they want to place
+        while True:
+            try:
+                num_ships = int(input("How many ships would you like to place? (1-5): "))
+                if num_ships < 1 or num_ships > 5:
+                    print("Enter a number from 1 to 5.")
+                    continue
+                break
+            except ValueError:
+                print("Invalid input. Please enter a number from 1 to 5.")
+
+        # calls place_ship so it can use data
+        for i in range(1, num_ships + 1):
+            print(f"Placing {self.ship_sizes[i]}")
+            self.place_ship(i + 1)
+
+    def turn(self, opponent_data):
+        # players turn to fire a shot
+        while True:
+            self.print_grids()
+            shot = input("Enter the coordinate to fire at (ex., B7): ")
+
+            try:
+                row, column = self.convert_to_index(shot)
+            except (ValueError, IndexError):
+                print("Invalid coordinate. Please try again.")
+                continue
+
+            if self.opponent_board[row][column] in ['X', '#']:
+                print("Cannot fire at coordinate again. Please try again.")
+            else:
+                # Call the opponent data callback to determine hit or miss
+                hit = opponent_data(row, column)
+                if hit:
+                    self.opponent_board[row][column] = 'X'  # Hit
+                    print("Hit!")
+                else:
+                    self.opponent_board[row][column] = '#'  # Miss
+                    print("Miss!")
+                break
+
+    def check_victory(self, board):
+        # check if all ships on one side have been sunk
         for row in board:
-            print("%2d|%s|" % (row_num, " ".join(row)))
-            row_num += 1
-    else:
-        print("%d|%s|" % (row_num, " ".join(row)))
+            if '^' or '<' in row:
+                return False  # there are still ships
+        return True  # all ships are sunk       
 
-    print("  +-+-+-+-+-+-+-+-+-+-+")
+    def connect(self):
+        # placeholder for opponent connection
+        input("Press Enter to connect to another player...")
 
-#test
-#print_grid(player_1_board)
-#print_grid(player_2_board)
+    def start_game(self, opponent_data_callback):
+        # starts game and runs loop
+        print("Welcome to Battleship!")
+        self.connect()
+        self.place_ships()
 
-def place_ships(board):
-    #go through each size of ship and place them
-    #prompt user to choose between placing horizontally or vertically
-    #then ask for starting position
+        # main loop
+        game_over = False
+        while not game_over:
+            # players turn
+            print("\nYour turn to fire!")
+            self.take_turn(opponent_data_callback) #uses opponent data for hit/miss
 
-    while True:
-        #place_ships == True
-        for length in ship_length:
-        #loop until ship fits and doesn't overlap
-            while True:
-                place_ships = True
-                print('Place the ship with a length of ' + str(length))
-                row, column, placing = user_input(place_ships)
-                if check_size_valid(length, row, column, placing):
-                    #check if ship overlaps
-                        if check_pos_overlap(board, row, column, placing, length) == False:
-                            #place ship
-                            if placing == "H":
-                                for i in range(column, column + length):
-                                    board[row][i] = "^"
-                            else:
-                                for i in range(row, row + length):
-                                    board[i][column] = "<"
-                            print_grid(player_1_board)
-                            #break 
+            # check for victory
+            if self.check_victory(self.opponent_board):
+                print("Congratulations, you've sunk all the opponent's ships! You win!")
+                game_over = True
+                break
 
+            # if not victory then next person goes
+            print("\nOpponent's turn to fire!")
 
-def check_size_valid(ship_length, row, column, placing):
-    #if placing horizontal, if the boat does not fit in the spot, return false
-    if placing == "H":
-        if column + ship_length > 8:
-            return False
-        else:
-            return True
-    #if placing vertical , if the boat does not fit in the spot, return false
-    else:
-        if row + ship_length > 8:
-            return False
-        else:
-            return True
+            # opponent shot
+            opponent_shot_result = self.receive_opponent_shot()
+
+            if opponent_shot_result == 'hit':
+                print("Opponent hit your ship!")
+            elif opponent_shot_result == 'miss':
+                print("Opponent missed!")
+            else:
+                print("Invalid shot. Opponent will go again.")
+
+            # check opponent victory
+            if self.check_victory(self.board):
+                print("All your ships have been sunk. Opponent wins!")
+                game_over = True
+                break
 
 
-def check_pos_overlap(board, row, column, placing, ship_length):
-    #if placing horizontally
-    #for the length of the boat in the column (for as many posistions as it will take in the column)
-    #check that there are no other boats placed there
-    #same goes for vertical check but it checks row instead of column duh
-    if placing == "H":
-        for i in range(column, column + ship_length):
-            if board[row][i] == "<" or "^":
-                return True
-    else:
-        for i in range(row, row + ship_length):
-            if board[i][column] == "^" or "<":
-                return True
-    return False
+# calling in opponents data
+def opponent_data(row, col):
+    # this is just tester data
+    # this is where the opponent data will be called into the game
+    opponent_ships = [
+        (1, 2), (1, 3), (1, 4), (4, 5), (6, 7),  
+    ]
+    return (row, col) in opponent_ships
 
 
-def user_input(place_ships):
-    #user input for placing their ships
-    if place_ships == True:
-        while True:
-            try:
-                placing = input("Enter direction to place ship (H or V): ").upper()
-                if placing == "H" or placing == "V":
-                    break
-            except TypeError:
-                print("Enter a proper placing, H (horizontal) or V (vertical)")
-        while True:
-            try:
-                row = int(input("Enter the row (1-10) for your ship: "))
-                if row <= 10:
-                    row = int(row) - 1
-                    break
-            except ValueError:
-                print("Enter a valid row between 1 and 10: ")
-        while True:
-            try:
-                column = input("Enter the column (A-J) for your ship").upper()
-                if column in "ABDCEFGHIJ":
-                    column = letters_to_numbers[column]
-                    break
-            except KeyError:
-                print("Enter a valid column from A to J: ")
-        return row, column, placing
-    
-    #user input for guessing
-    else:
-        while True:
-            try: 
-                row = int(input("Enter the row (1-10) of the ship: "))
-                if row <=10:
-                    row = int(row) - 1
-                    break
-            except ValueError:
-                print('Enter a valid row between 1 and 10: ')
-        while True:
-            try: 
-                column = input("Enter the column (A-J) of the ship: ").upper()
-                if column in 'ABCDEFGHIJ':
-                    column = letters_to_numbers[column]
-                    break
-            except KeyError:
-                print('Enter a valid column from A to J: ')
-        return row, column 
-
-
-
-def ships_hit():
-    pass
-
-def turn(board):
-    pass
+if __name__ == "__main__":
+    game = BattleshipView()
+    game.start_game(opponent_data)
